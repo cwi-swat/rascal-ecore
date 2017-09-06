2ec733505d5547ddacae9a770f793233acf1fe8e
Thomas Degueule <degueule@cwi.nl>
2017-09-06 18:03:20 +0200

    Cleaning a bit

--- src/lang/ecore/IO.java
+++ src/lang/ecore/IO.java
@@ -17,7 +17,6 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.rascalmpl.interpreter.TypeReifier;
-import org.rascalmpl.interpreter.utils.RuntimeExceptionFactory;
 
 import io.usethesource.vallang.IConstructor;
 import io.usethesource.vallang.ISourceLocation;
@@ -60,7 +59,7 @@ public class IO {
 		return visit(root, rt, ts);
 	}
 
-	public EObject loadModel(String uri) {
+	private EObject loadModel(String uri) {
 		ResourceSet rs = new ResourceSetImpl();
 		rs.getResourceFactoryRegistry().getExtensionToFactoryMap()
 			.put("*", new XMIResourceFactoryImpl());
@@ -75,37 +74,41 @@ public class IO {
 		if (obj instanceof EObject) {
 			EObject eObj = (EObject) obj;
 			EClass eCls = eObj.eClass();
+
+			// FIXME: Assuming that there's a unique constructor with the EClass' name
 			Type t = ts.lookupConstructor(type, toFirstLowerCase(eCls.getName())).iterator().next();
 			
 			List<IValue> fields = new ArrayList<>();
-			for(int i = 0; i < t.getArity(); i++) {
-				//Rascal side
+			for (int i = 0; i < t.getArity(); i++) {
+				// Rascal side
 				String fieldName = t.getFieldName(i);
 				Type fieldType = t.getFieldType(i);
 				
-				//EMF side
+				// EMF side
 				EStructuralFeature feature = eCls.getEStructuralFeature(fieldName);
 				Object featureValue = eObj.eGet(feature);
-				
-				if(feature instanceof EReference) {
+
+				if (feature instanceof EReference) {
+					// Then featureValue is an EObject
 					EReference ref = (EReference) feature;
-					if(ref.isContainment()) {
-						fields.add(visitContainmentRef(ref,featureValue, fieldType,ts));
+					if (ref.isContainment()) {
+						fields.add(visitContainmentRef(ref, featureValue, fieldType, ts));
 					}
 					else {
-						fields.add(visitReference(ref,(EObject) featureValue, fieldType));
+						fields.add(visitReference(ref, featureValue, fieldType));
 					}
 				}
-				else if(feature instanceof EAttribute) {
+				else if (feature instanceof EAttribute) {
+					// Then featureValue is a primitive type
 					EAttribute att = (EAttribute) feature;
-					fields.add(visitAttribute(att,featureValue, fieldType,ts));
+					fields.add(visitAttribute(att, featureValue, fieldType, ts));
 				}
 			}
 			
 			Map<String,IValue> keywords = new HashMap<>();
 			keywords.put("uid", getOrCreateId(eObj));
 			IValue[] arr = new IValue[fields.size()];
-			return vf.constructor(t,fields.toArray(arr),keywords);
+			return vf.constructor(t, fields.toArray(arr), keywords);
 		}
 		else {
 			return makePrimitive(obj);
@@ -114,141 +117,106 @@ public class IO {
 	
 	
 	/**
-	 * Build ADT by visiting an EAttribute
+	 * Returns IValue for an EAttribute
 	 */
 	private IValue visitAttribute(EStructuralFeature ref, Object refValue, Type fieldType, TypeStore ts) {
-		
-		if(ref.isMany() && ref.isUnique() && ref.isOrdered()) {
-			// return ?
+		if (ref.isMany()) {
 			List<Object> refValues = (List<Object>) refValue;
-			Type elemType = fieldType.getElementType();
-			List<IValue> values = refValues.stream().map(elem -> visit(elem,elemType,ts)).collect(Collectors.toList());
+			List<IValue> values = refValues.stream().map(elem -> makePrimitive(refValue)).collect(Collectors.toList());
 			IValue[] arr = new IValue[values.size()];
-			return vf.list(values.toArray(arr));
+			IValue[] valuesArray = values.toArray(arr);
+
+			if (ref.isUnique()) {
+				if (ref.isOrdered()) {            // M & U & O = ?
+					return vf.list(valuesArray);
+				} else {                          // M & U & !O = Set[T]
+					return vf.set(valuesArray);
+				}
+			} else {
+				if (ref.isOrdered()) {            // M & !U & O = list[T]
+					return vf.list(valuesArray);
+				} else {                          // M & !U & !O = map[T, int]
+					// TODO: Implement me
+				}
+			}
+		} else {
+			if (!ref.isRequired()) {              // !M && O = Opt[T]
+				Type rt = ts.lookupAbstractDataType("Opt");
+				Type t = ts.lookupConstructor(rt, "just", tf.tupleType(makePrimitive(refValue)));
+				return vf.constructor(t);
+			} else {                              // !M && !O = T
+				return makePrimitive(refValue);
+			}
 		}
 
-		if (ref.isMany() && ref.isUnique() && !ref.isOrdered()) {
-			// return set[T]
-			List<Object> refValues = (List<Object>) refValue;
-			Type elemType = fieldType.getElementType();
-			List<IValue> values = refValues.stream().map(elem -> visit(elem,elemType,ts)).collect(Collectors.toList());
-			IValue[] arr = new IValue[values.size()];
-			return vf.set(values.toArray(arr));
-		}
-
-		if (ref.isMany() && !ref.isUnique() && ref.isOrdered()) {
-			// return list[T]
-			List<Object> refValues = (List<Object>) refValue;
-			Type elemType = fieldType.getElementType();
-			List<IValue> values = refValues.stream().map(elem -> visit(elem,elemType,ts)).collect(Collectors.toList());
-			IValue[] arr = new IValue[values.size()];
-			return vf.list(values.toArray(arr));
-		}
-
-		if (ref.isMany() && !ref.isUnique() && !ref.isOrdered()) {
-			// return map[T,int]
-			//TODO: vf.map(arg0, arg1)
-		}
-		
-		if(!ref.isRequired() && !ref.isMany()) {
-			// return Opt[T]
-			Type rt = ts.lookupAbstractDataType("Opt");
-			Type t = ts.lookupConstructor(rt, "just", tf.tupleType(visit(refValue,fieldType,ts)));
-			return vf.constructor(t);
-		}
-		
-		if(ref.isRequired() && !ref.isMany()) {
-			// return T
-			return makePrimitive(refValue);
-		}
-		
 		return null;
 	}
 	
 	/**
-	 * Build an ADT by visiting a containment EReference
+	 * Returns IValue for a containment EReference
 	 */
 	private IValue visitContainmentRef(EStructuralFeature ref, Object refValue, Type fieldType, TypeStore ts) {
-		
-		if(ref.isMany() && ref.isUnique() && ref.isOrdered()) {
-			// return ?
+		if (ref.isMany()) {
 			List<Object> refValues = (List<Object>) refValue;
 			Type elemType = fieldType.getElementType();
-			List<IValue> values = refValues.stream().map(elem -> visit(elem,elemType,ts)).collect(Collectors.toList());
+			List<IValue> values = refValues.stream().map(elem -> visit(elem, elemType, ts)).collect(Collectors.toList());
 			IValue[] arr = new IValue[values.size()];
-			return vf.list(values.toArray(arr));
-		}
-
-		if (ref.isMany() && ref.isUnique() && !ref.isOrdered()) {
-			// return set[T]
-			List<Object> refValues = (List<Object>) refValue;
-			Type elemType = fieldType.getElementType();
-			List<IValue> values = refValues.stream().map(elem -> visit(elem,elemType,ts)).collect(Collectors.toList());
-			IValue[] arr = new IValue[values.size()];
-			return vf.set(values.toArray(arr));
-		}
-
-		if (ref.isMany() && !ref.isUnique() && ref.isOrdered()) {
-			// return list[T]
-			List<Object> refValues = (List<Object>) refValue;
-			Type elemType = fieldType.getElementType();
-			List<IValue> values = refValues.stream().map(elem -> visit(elem,elemType,ts)).collect(Collectors.toList());
-			IValue[] arr = new IValue[values.size()];
-			return vf.list(values.toArray(arr));
-		}
-
-		if (ref.isMany() && !ref.isUnique() && !ref.isOrdered()) {
-			// return map[T,int]
-			//TODO: vf.map(arg0, arg1)
-		}
-		
-		if(!ref.isRequired() && !ref.isMany()) {
-			// return Opt[T]
-			Type rt = ts.lookupAbstractDataType("Opt");
-			Type t = ts.lookupConstructor(rt, "just", tf.tupleType(visit(refValue,fieldType,ts)));
-			return vf.constructor(t);
-		}
-		
-		if(ref.isRequired() && !ref.isMany()) {
-			// return T
-			Type t = ts.lookupConstructor(fieldType, toFirstLowerCase(fieldType.getName()), tf.tupleType(visit(refValue,fieldType,ts)));
-			return vf.constructor(t);
+			IValue[] valuesArray = values.toArray(arr);
+			
+			if (ref.isUnique()) {
+				if (ref.isOrdered()) {            // M & U & O = ?
+					return vf.list(values.toArray(valuesArray));
+				} else {                          // M & U & !O = set[T]
+					return vf.set(values.toArray(valuesArray));
+				}
+			} else {
+				if (ref.isOrdered()) {            // M & !U & O = list[T]
+					return vf.list(values.toArray(valuesArray));
+				} else {                          // M & !U & !O = map[T, int]
+					// TODO: Implement me
+				}
+			}
+		} else {
+			if (!ref.isRequired()) {              // !M && O = Opt[T]
+				Type rt = ts.lookupAbstractDataType("Opt");
+				Type t = ts.lookupConstructor(rt, "just", tf.tupleType(visit(refValue, fieldType, ts)));
+				return vf.constructor(t);
+			} else {                              // !M && !O = T
+				Type t = ts.lookupConstructor(fieldType, toFirstLowerCase(fieldType.getName()), tf.tupleType(visit(refValue, fieldType, ts)));
+				return vf.constructor(t);
+			}
 		}
 		
 		return null;
 	}
 	
 	/**
-	 * Build an ADT by visting an EReference
+	 * Returns IValue for an EReference
 	 */
-	private IValue visitReference(EReference ref, EObject refValue, Type fieldType) {
-		
-		if(ref.isMany() && ref.isUnique() && !ref.isOrdered()) {
-			// return set[Ref[T]]
+	private IValue visitReference(EReference ref, Object refValue, Type fieldType) {
+		if (ref.isMany()) {
 			List<EObject> refValues = (List<EObject>) refValue;
 			List<IValue> valuesToRef = refValues.stream().map(elem -> makeRefTo(elem)).collect(Collectors.toList());
 			IValue[] arr = new IValue[valuesToRef.size()];
-			return vf.set(valuesToRef.toArray(arr));
+			IValue[] valuesArray = valuesToRef.toArray(arr);
+
+			if (ref.isUnique()) {
+				if (ref.isOrdered()) {            // M & U & O = ?
+					// TODO: Implement me
+				} else {                          // M & U & !O = set[Ref[T]]
+					return vf.set(valuesArray);
+				}
+			} else {
+				if (ref.isOrdered()) {            // M & !U & O = list[Ref[T]]
+					return vf.list(valuesArray);
+				} else {                          // M & !U & !O = Map[Ref[T], int]
+					// TODO: Implement me
+				}
+			}
+		} else {
+			return makeRefTo((EObject) refValue);
 		}
 
-		if (ref.isMany() && !ref.isUnique() && ref.isOrdered()) {
-			// return list[Ref[T]]
-			List<EObject> refValues = (List<EObject>) refValue;
-			List<IValue> valuesToRef = refValues.stream().map(elem -> makeRefTo(elem)).collect(Collectors.toList());
-			IValue[] arr = new IValue[valuesToRef.size()];
-			return vf.list(valuesToRef.toArray(arr));
-		}
-
-		if (ref.isMany() && !ref.isUnique() && !ref.isOrdered()) {
-			// return map[Ref[T],int]
-			//TODO:
-		}
-
-		if (!ref.isMany()) {
-			// return Ref[T]
-			return makeRefTo(refValue);
-		}
-		
 		return null;
 	}
 	
@@ -258,10 +226,9 @@ public class IO {
 	private IValue makeId(int num) {
 		TypeStore ts = new TypeStore();
 		Type idType = tf.abstractDataType(ts, "Id");
-		
 		Type id_int = tf.constructor(ts, idType, "id", tf.integerType());
 		
-		return vf.constructor(id_int,vf.integer(num));
+		return vf.constructor(id_int, vf.integer(num));
 	}
 	
 	/**
@@ -269,7 +236,7 @@ public class IO {
 	 */
 	private IValue getOrCreateId(EObject obj) {
 		Integer uid = eObjectToUid.get(obj);
-		if(uid == null) {
+		if (uid == null) {
 			uid = COUNTER++;
 			eObjectToUid.put(obj,uid);
 		}
@@ -280,13 +247,12 @@ public class IO {
 	 * Return ref(id(Num)) or none() if {@link eObj} is null
 	 */
 	private IValue makeRefTo(EObject eObj) {
-		
 		TypeStore ts = new TypeStore();
 		
-		if(eObj ==  null) {
+		if (eObj == null) {
 			Type optType = tf.abstractDataType(ts, "Opt");
 			Type none = tf.constructor(ts, optType, "none");
-			return vf.constructor(none,new IValue[0]);
+			return vf.constructor(none, new IValue[0]);
 		}
 		
 		Type idType = tf.abstractDataType(ts, "Id");
@@ -299,44 +265,43 @@ public class IO {
 	}
 	
 	/**
-	 * Return ADT for primitive
+	 * Returns IValue for primitive type
 	 */
 	private IValue makePrimitive(Object obj) {
-		
-		if(obj instanceof Boolean) {
-			return vf.bool((boolean) obj);
+		if (obj instanceof Boolean) {
+			return vf.bool((Boolean) obj);
 		}
-		else if(obj instanceof Byte) {
-			//FIXME
+		else if (obj instanceof Byte) { // FIXME: Rascal's byte?
 		}
-		else if(obj instanceof Character) {
-			//FIXME:
-			//vf.string(((Character)obj).toString());
+		else if (obj instanceof Character) { // FIXME: Rascal's char?
+			return vf.string(Character.toString((Character) obj));
 		}
-		else if(obj instanceof Double) {
-			return vf.real((String) obj);
+		else if (obj instanceof Double) { // FIXME: Rascal's double?
+			return vf.real((Double) obj);
 		}
-		else if(obj instanceof Integer) {
-			return vf.integer((String) obj);
+		else if (obj instanceof Integer) {
+			return vf.integer((Integer) obj);
 		}
-		else if(obj instanceof Long) {
-			return vf.integer((String) obj);
+		else if (obj instanceof Long) { // FIXME: Rascal's long?
+			return vf.integer((Long) obj);
 		}
-		else if(obj instanceof Short) {
-			return vf.integer((int)obj);
+		else if (obj instanceof Short) { // FIXME: Rascal's short?
+			return vf.integer((Short)obj);
 		}
-		else if(obj instanceof Float) {
-			return vf.real((double)obj);
+		else if (obj instanceof Float) { // FIXME: Rascal's float?
+			return vf.real((Float)obj);
 		}
-		else if(obj instanceof String) {
+		else if (obj instanceof String) {
 			return vf.string((String) obj);
 		}
-		//FIXME: manage enum
+
+		// FIXME: Enums?
+		// FIXME: Datatypes?
 		
 		return null;
 	}
 	
 	private String toFirstLowerCase(String s) {
-		return s.substring(0, 1).toUpperCase() + s.substring(1);
+		return s.substring(0, 1).toLowerCase() + s.substring(1);
 	}
 }
